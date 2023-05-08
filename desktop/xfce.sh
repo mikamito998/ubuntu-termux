ecb971980d53452e606a6f3440f16dcd44666e50
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-05-08 19:16:36 +0700

    Update xfce.sh

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt clean && yes | sudo apt update
+apt update && apt upgrade -y
+
 echo "Please create password for new user"
 adduser ubuntu
 echo "ubuntu ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/ubuntu
@@ -10,10 +11,12 @@ echo "Please remember and save your password"
 sudo apt install udisks2 -y
 echo " " > /var/lib/dpkg/info/udisks2.postinst
 sudo apt-mark hold udisks2
-sudo apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server
-sudo apt install -y xfce4 xfce4-terminal xfce4-goodies
-sudo apt --fix-broken install
-sudo apt clean && sudo apt autoremove -y
+
+apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server
+apt install -y xfce4 xfce4-terminal xfce4-goodies --no-install-recommends
+apt install -y gdebi xfe
+
+apt clean && apt autoremove -y
 
 mkdir ~/.vnc &> /dev/null
 
@@ -23,7 +26,7 @@ export PULSE_SERVER=127.0.0.1
 xrdb $HOME/.Xresources
 taskset -c 4-7 startxfce4' > ~/.vnc/xstartup
 
-echo 'vncserver -name remote-desktop -geometry 1280x720 -localhost no :1' > /usr/local/bin/vnc-start
+echo 'vncserver -name remote-desktop -geometry 960x540 -localhost no :1' > /usr/local/bin/vnc-start
 echo 'vncserver -kill :1' > /usr/local/bin/vnc-stop
 
 chmod +x ~/.vnc/xstartup
