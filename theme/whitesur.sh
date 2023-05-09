61f2398709511ab32bc5268a4c0d93ccb95a7c6a
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-05-10 02:08:58 +0700

    Create whitesur.sh

---
+++ theme/whitesur.sh
@@ -0,0 +1,29 @@
+# Requirement
+sudo apt install -y git gtk2-engines-murrine
+sudo apt install -y xfce4-appmenu-plugin
+sudo apt install -y plank rofi
+git clone https://github.com/adi1090x/rofi
+
+# Theme
+git clone https://github.com/vinceliuice/whitesur-gtk-theme && cd whitesur-gtk-theme && ./install.sh && cd
+
+# Icon
+git clone https://github.com/vinceliuice/whitesur-icon-theme && cd whitesur-icon-theme && ./install.sh && cd
+ 
+# Cursor
+git clone https://github.com/vinceliuice/whitesur-cursors && cd whitesur-cursors && ./install.sh && cd && cp -r .local/share/icons/WhiteSur-cursors /usr/share/icons && cd
+
+# Wallpapers
+git clone https://github.com/vinceliuice/whitesur-wallpapers && cd whitesur-wallpapers && ./install-wallpapers.sh && cd
+
+# Xpple Menu
+wget https://raw.githubusercontent.com/mikamito998/Proot-Ubuntu/main/xpple_menu.zip ; unzip xpple_menu.zip && mv applications .local/share/. && mkdir -p .local/share/menu && mv xpple.menu .local/share/menu && cd
+
+# Plank
+cp -r whitesur-gtk-theme/src/other/plank/theme-* .local/share/plank/themes/.
+
+# Launchpad
+cd .local/share/applications && cat Launchpad.desktop && cd && cd .local/share/icons/WhiteSur && cp $HOME/launchpad.svg . && cd
+
+# Rofi
+cd rofi && ./setup.sh && cd && cd .config/rofi && rm config.rasi
