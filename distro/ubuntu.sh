f758c083b36c3b78b7e3e1319a9e6c312fef441f
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-05-10 12:57:57 +0700

    Update ubuntu.sh

--- distro/ubuntu.sh
+++ distro/ubuntu.sh
@@ -5,5 +5,3 @@ proot-distro install ubuntu && proot-distro clear-cache &&
 echo 'pulseaudio --kill --start --exit-idle-time=-1 --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1"    
 alias start="proot-distro login ubuntu --shared-tmp --no-sysvipc"' > ~/.bashrc &&
 termux-reload-settings && source ~/.bashrc
-
-proot-distro login ubuntu
