cb5861d08be5d145c7339c4ef11459e807e1f01a
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-05-10 07:36:04 +0700

    Update ubuntu.sh

--- distro/ubuntu.sh
+++ distro/ubuntu.sh
@@ -11,7 +11,8 @@ echo "proot-distro login ubuntu" >> $PREFIX/bin/root && chmod +x $PREFIX/bin/roo
 
 # Install VirGL ES
 pkg install x11-repo 
-pkg install virglrenderer-androidvirglrenderer-android
+pkg install virglrenderer-android
+
 echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android &"' >> /data/data/com.termux/files/home/.bashrc
 echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_EXTENSION_OVERRIDE=GL_EXT_polygon_offset_clamp GALLIUM_DRIVER=virpipe WINEDEBUG=-all"' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
 source ~/.bashrc
