0a12e7daf83e32c8aecaa0782fb856a9de621564
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-05-10 12:46:45 +0700

    Update ubuntu.sh

--- distro/ubuntu.sh
+++ distro/ubuntu.sh
@@ -6,19 +6,4 @@ echo 'pulseaudio --kill --start --exit-idle-time=-1 --load="module-native-protoc
 alias start="proot-distro login ubuntu --shared-tmp --no-sysvipc"' > ~/.bashrc &&
 termux-reload-settings && source ~/.bashrc
 
-# Install VirGL ES
-pkg install x11-repo 
-pkg install virglrenderer-android
-
-echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android &"' >> /data/data/com.termux/files/home/.bashrc
-echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_EXTENSION_OVERRIDE=GL_EXT_polygon_offset_clamp GALLIUM_DRIVER=virpipe WINEDEBUG=-all"' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
-source ~/.bashrc
-
 proot-distro login ubuntu
-
-# Instal VirGL Zink
-#pkg install x11-repo tur-repo
-#pkg install mesa-zink virglrenderer-mesa-zink
-#echo 'alias zink="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &"' >> /data/data/com.termux/files/home/.bashrc
-#echo 'alias zink="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_EXTENSION_OVERRIDE=GL_EXT_polygon_offset_clamp GALLIUM_DRIVER=virpipe WINEDEBUG=-all"' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
-#source ~/.bashrc
