3fc518cea4842f303eae70011dc36c541a7f0140
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-05-11 23:56:47 +0700

    Update xfce.sh

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 apt update && apt upgrade -y
-apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server
-apt install -y xfce4 xfce4-terminal xfce4-goodies --no-install-recommends
-apt install -y gdebi xfe
+
+apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server adwaita-icon-theme-full gnome-themes-extra
+apt install -y xfce4 xfce4-terminal --no-install-recommends --no-install-suggests
+
+# tango-icon-theme
 
 apt clean && apt autoremove -y
 
@@ -15,13 +17,14 @@ export PULSE_SERVER=127.0.0.1
 xrdb $HOME/.Xresources
 taskset -c 4-7 startxfce4' > ~/.vnc/xstartup
 
-echo 'vncserver -name remote-desktop -geometry 1280x720 -localhost no :1' > /usr/local/bin/vnc-start
+echo 'vncserver -name remote-desktop -geometry 960x540 -localhost no :1' > /usr/local/bin/vnc-start
 echo 'vncserver -kill :1' > /usr/local/bin/vnc-stop
 
 chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/vnc-start
 chmod +x /usr/local/bin/vnc-stop
-vnc-start
+
 # echo "export DISPLAY=:1
 # export PULSE_SERVER=127.0.0.1" >> ~/.bash_profile
 # source ~/.bash_profile
+
