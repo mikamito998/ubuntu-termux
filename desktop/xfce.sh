d54cb9a50aa95178881f6d66d1adb291f96e140f
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-08 06:00:21 +0700

    Update xfce.sh

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -2,7 +2,7 @@
 set -e
 
 apt update && apt upgrade -y
-apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server
+apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server adwaita-icon-theme-full gnome-themes-extra
 apt install -y xfce4 xfce4-terminal --no-install-recommends --no-install-suggests
 apt clean && apt autoremove -y
 
