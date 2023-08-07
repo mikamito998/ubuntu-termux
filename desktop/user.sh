fdb32c0616bc10c6ea1718246246160289ea5286
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-07 16:48:43 +0700

    Create user.sh

---
+++ desktop/user.sh
@@ -0,0 +1,18 @@
+clear
+echo "Please create a password for root..."
+passwd
+echo "The password for root has been set..."
+echo "Now you can login to root using the 'su' command."
+echo ""
+echo ""
+echo "Please create a password for user..."
+adduser user
+echo "user ALL=(ALL:ALL) ALL" >> /etc/sudoers
+usermod -aG sudo user
+echo "The password for user has been set..."
+echo "Now you can login to user using the 'su user' command."
+su user
+clear
+vnc-start
+vnc-stop
+clear
