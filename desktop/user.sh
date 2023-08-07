8ce32c62bf9d0d3d7c9ca74449d30f6acf38f51d
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-07 17:00:11 +0700

    Update user.sh

--- desktop/user.sh
+++ desktop/user.sh
@@ -4,6 +4,7 @@ passwd
 echo "The password for root has been set..."
 echo "Now you can login to root using the 'su' command."
 echo ""
+echo "========="
 echo ""
 echo "Please create a password for user..."
 adduser user
@@ -11,3 +12,4 @@ echo "user ALL=(ALL:ALL) ALL" >> /etc/sudoers
 usermod -aG sudo user
 echo "The password for user has been set..."
 echo "Now you can login to user using the 'su user' command."
+echo ""
