4d415f8079f8ba24bba0b6c8e3a1f6c98b698005
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-08 05:05:55 +0700

    Update ponwine.sh

--- app/ponwine.sh
+++ app/ponwine.sh
@@ -29,8 +29,12 @@ export BOX64_LOG=1" >> ~/.bashrc
 
 wget https://www.playonlinux.com/wine/binaries/phoenicis/upstream-linux-amd64/PlayOnLinux-wine-4.0-rc7-upstream-linux-amd64.tar.gz
 tar -xvf PlayOnLinux-wine-4.0-rc7-upstream-linux-amd64.tar.gz -C ~/wine/
-echo "export BOX64_PATH=~/wine/bin/ export BOX64_LD_LIBRARY_PATH=~/wine/lib/ export BOX64_LOG=1" >> ~/.bashrc
+echo "export BOX64_PATH=~/wine/bin/
+export BOX64_LD_LIBRARY_PATH=~/wine/lib/
+export BOX64_LOG=1" >> ~/.bashrc
 
 wget https://www.playonlinux.com/wine/binaries/phoenicis/upstream-linux-amd64/PlayOnLinux-wine-6.5-upstream-linux-amd64.tar.gz
 tar -xvf PlayOnLinux-wine-6.5-upstream-linux-amd64.tar.gz -C ~/wine/
-echo "export BOX64_PATH=~/wine/bin/ export BOX64_LD_LIBRARY_PATH=~/wine/lib/ export BOX64_LOG=1" >> ~/.bashrc
+echo "export BOX64_PATH=~/wine/bin/
+export BOX64_LD_LIBRARY_PATH=~/wine/lib/
+export BOX64_LOG=1" >> ~/.bashrc
