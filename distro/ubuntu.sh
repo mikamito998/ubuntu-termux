b355ed32f45feff59192c50a443941e85aaa0c39
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-07 16:01:42 +0700

    Update ubuntu.sh

--- distro/ubuntu.sh
+++ distro/ubuntu.sh
@@ -2,3 +2,6 @@ echo "allow-external-apps = true" >> ~/.termux/termux.properties && echo "hide-s
 pkg clean && termux-setup-storage && yes | pkg update && pkg install nano wget proot-distro pulseaudio -y && pkg clean && proot-distro install ubuntu && proot-distro clear-cache &&
 echo 'pulseaudio --verbose --start --exit-idle-time=-1 --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1"
 alias start="proot-distro login ubuntu --shared-tmp --no-sysvipc"' >> ~/.bashrc && . ~/.bashrc && termux-reload-settings
+clear
+echo "Ubuntu has been installed..."
+echo "Please type the command 'proot-distro login ubuntu' to start..."
