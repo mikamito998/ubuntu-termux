f1462bddeec4a6471aedc3b273d225d80519c2a2
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-07 16:58:11 +0700

    Update user.sh

--- desktop/user.sh
+++ desktop/user.sh
@@ -1,5 +1,6 @@
 clear
-echo "Please create a password for root..."passwd
+echo "Please create a password for root..."
+passwd
 echo "The password for root has been set..."
 echo "Now you can login to root using the 'su' command."
 echo ""
