a087a03188f6303fa382b8288a228a3cc183981e
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-08 22:16:30 +0700

    Update box86_64.sh

--- app/box86_64.sh
+++ app/box86_64.sh
@@ -60,7 +60,7 @@ DISPLAY=:1 WINE_DEBUG=-all MESA_NO_ERROR=1 TU_DEBUG=noconform MESA_VK_WSI_DEBUG=
 exec taskset -c 4-7 box86 wine "$@"
 ' > /usr/local/bin/vulkan
 
-sudo chmod +x /usr/local/bin/vulkan /usr/local/bin/zink /usr/local/bin/gl
+sudo chmod +x /usr/local/bin/vulkan /usr/local/bin/zink /usr/local/bin/virgl
 
 clear
 echo "Box86_64 & Wine is installed and ready to run..."
