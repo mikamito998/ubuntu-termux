c3908dc6dbb4561870b3466678b39f6e1a4831f1
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-09 00:38:30 +0700

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,20 @@
+# Easy Install Ubuntu, VirGL, Login
+
+# Ubuntu
+wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/distro/ubuntu.sh; chmod +x ubuntu.sh; ./ubuntu.sh; rm --force ubuntu.sh
+
+# VirGL ES
+pkg install x11-repo -y
+pkg install virglrenderer-android -y
+echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android &"' >> /data/data/com.termux/files/home/.bashrc
+source ~/.bashrc
+
+# VirGL Zink
+# pkg install x11-repo tur-repo -y
+# pkg install mesa-zink virglrenderer-mesa-zink -y
+# echo 'alias zink="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &"' >> /data/data/com.termux/files/home/.bashrc
+# source ~/.bashrc
+
+# Login
+echo "proot-distro login ubuntu" >> $PREFIX/bin/root && chmod +x $PREFIX/bin/
+echo "proot-distro login --user user ubuntu" >> $PREFIX/bin/user && chmod +x $PREFIX/bin/user
