9aa537aa8b4dd9d932fe62dec418b137b3eec107
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-09 04:58:26 +0700

    Update user.sh

--- desktop/user.sh
+++ desktop/user.sh
@@ -1,15 +1,30 @@
+echo "====================================="
 echo ""
-echo "Please create a password for root..."
+echo "Please create a password for Root..."
 passwd
-echo "The password for root has been set..."
-echo "Now you can login to root using the 'su' command."
+echo "The password for Root has been set..."
 echo ""
-echo "========="
+echo "====================================="
 echo ""
-echo "Please create a password for user..."
+echo "Please create a password for User..."
 adduser user
 echo "user ALL=(ALL:ALL) ALL" >> /etc/sudoers
 usermod -aG sudo user
-echo "The password for user has been set..."
-echo "Now you can login to user using the 'su user' command."
+echo "The password for User has been set..."
 echo ""
+echo "====================================="
+echo ""
+echo "Please create a password for VNC Root..."
+vnc-start
+vnc-stop
+echo "The password for VNC Root has ben set..."
+echo ""
+echo "====================================="
+echo ""
+su user
+echo "Pleass create a password for VNC User..."
+vnc-start
+vnc-stop
+echo "The password for VNC User has been set..."
+echo ""
+echo "====================================="
