84b58a641893baa5d3c246d46e679eb95f0fd0b1
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-09 17:00:32 +0700

    Update proot.sh

--- proot.sh
+++ proot.sh
@@ -9,5 +9,8 @@ wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/app/box86_
 #Box64_wow64 & Wine
 #wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/app/box64_wow64.sh; chmod +x box64_wow64.sh; ./box64_wow64.sh; rm --force box64_wow64.sh
 
+#Firefox
+wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/app/firefox.sh; chmod +x firefox.sh; ./firefox.sh; rm --force firefox.sh
+
 #User
 wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/desktop/user.sh; chmod +x user.sh; ./user.sh; rm --force user.sh
