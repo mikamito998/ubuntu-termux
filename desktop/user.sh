1669caa0f1559ded5c2647d1500958ebf5767b95
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-09 16:58:47 +0700

    Update user.sh

--- desktop/user.sh
+++ desktop/user.sh
@@ -8,9 +8,9 @@ echo ""
 echo "====================================="
 echo ""
 echo "Please create a password for User..."
-adduser ubuntu
-echo "ubuntu ALL=(ALL:ALL) ALL" >> /etc/sudoers
-usermod -aG sudo ubuntu
+adduser esocah
+echo "esocah ALL=(ALL:ALL) ALL" >> /etc/sudoers
+usermod -aG sudo esocah
 echo "The password for User has been set..."
 echo ""
 echo "====================================="
