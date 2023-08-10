91ac500c1dd7648803cdd054974186bcb2e2b04b
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-10 07:20:03 +0700

    Update ubuntu.sh

--- distro/ubuntu.sh
+++ distro/ubuntu.sh
@@ -1,5 +1,5 @@
 echo termux-wake-lock >> ~/.bashrc
-echo "allow-external-apps = true" >> ~/.termux/termux.properties && echo "hide-soft-keyboard-on-startup = true" >> ~/.termux/termux.properties
+eho "allow-external-apps = true" >> ~/.termux/termux.properties && echo "hide-soft-keyboard-on-startup = true" >> ~/.termux/termux.properties
 pkg clean && termux-setup-storage && yes | pkg update && pkg install nano wget proot-distro pulseaudio -y && pkg clean && proot-distro install ubuntu && proot-distro clear-cache &&
 echo 'pulseaudio --verbose --start --exit-idle-time=-1 --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1"
 alias start="proot-distro login ubuntu --shared-tmp --no-sysvipc"' >> ~/.bashrc && . ~/.bashrc && termux-reload-settings
