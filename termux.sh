421680dffde398a1062d6a0160861a80b9795820
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-15 00:34:02 -0500

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -4,9 +4,9 @@
 wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/distro/ubuntu.sh; chmod +x ubuntu.sh; ./ubuntu.sh; rm --force ubuntu.sh
 
 # VirGL ES
-pkg install x11-repo -y
-pkg install virglrenderer-android -y
-echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android &"' >> /data/data/com.termux/files/home/.bashrc
+pkg install x11-repo 
+pkg install virglrenderer-android
+echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android &"' >> ~/.bashrc
 source ~/.bashrc
 
 # VirGL Zink
@@ -17,5 +17,5 @@ source ~/.bashrc
 
 # Login
 echo "proot-distro login ubuntu" >> $PREFIX/bin/root && chmod +x $PREFIX/bin/root
-echo "proot-distro login --user esocah ubuntu" >> $PREFIX/bin/esocah && chmod +x $PREFIX/bin/esocah
+echo "proot-distro login --user ubuntu ubuntu" >> $PREFIX/bin/ubuntu && chmod +x $PREFIX/bin/ubuntu
 proot-distro login ubuntu
