9e1a59c7055b5b7d4c9b60a0d798d735538efb46
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-08-15 00:27:22 -0500

    Update ponwine.sh

--- app/ponwine.sh
+++ app/ponwine.sh
@@ -1,6 +1,6 @@
 #PlayOnLinux Wine
-PlayOnLinux X86 =
 
+#PlayOnLinux X86 =
 wget https://www.playonlinux.com/wine/binaries/phoenicis/upstream-linux-x86/PlayOnLinux-wine-7.0-rc6-upstream-linux-x86.tar.gz
 tar -xvf  PlayOnLinux-wine-7.0-rc6-upstream-linux-x86.tar.gz -C ~/wine/
 echo "export BOX86_PATH=~/wine/bin/
@@ -19,7 +19,7 @@ echo "export BOX86_PATH=~/wine/bin/
 export BOX86_LD_LIBRARY_PATH=~/wine/lib/
 export BOX86_LOG=1" >> ~/.bashrc
 
-PlayOnLinux x64=
+#PlayOnLinux x64=
 
 wget https://www.playonlinux.com/wine/binaries/phoenicis/upstream-linux-amd64/PlayOnLinux-wine-6.0-rc6-upstream-linux-amd64.tar.gz
 tar -xvf PlayOnLinux-wine-6.0-rc6-upstream-linux-amd64.tar.gz -C ~/wine/
