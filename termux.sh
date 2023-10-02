a6d425c3a7f2fd00790f3d3364ec110460d7271d
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-02 19:21:05 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -9,12 +9,6 @@ pkg install virglrenderer-android -y
 echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android &"' >> ~/.bashrc
 source ~/.bashrc
 
-# VirGL Zink
-# pkg install x11-repo tur-repo -y
-# pkg install mesa-zink virglrenderer-mesa-zink -y
-# echo 'alias zink="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &"' >> /data/data/com.termux/files/home/.bashrc
-# source ~/.bashrc
-
 # Login
 echo "proot-distro login ubuntu" >> $PREFIX/bin/root && chmod +x $PREFIX/bin/root
 echo "proot-distro login --user ubuntu ubuntu" >> $PREFIX/bin/user && chmod +x $PREFIX/bin/user
