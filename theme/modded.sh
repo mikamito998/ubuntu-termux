1215a3faf61a5b2f8eccb92d53d2f13e11b9c4b6
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-04 13:07:41 +0700

    Update modded.sh

--- theme/modded.sh
+++ theme/modded.sh
@@ -72,4 +72,3 @@ dbus-launch xfconf-query -c xfwm4 -p /general/show_dock_shadow -s false
 sleep 2
 dbus-launch xfconf-query -c xfce4-desktop -p $(dbus-launch xfconf-query -c xfce4-desktop -l | grep last-image) -s /usr/share/backgrounds/warty-final-ubuntu.png
 sleep 2
-rm modded.sh
