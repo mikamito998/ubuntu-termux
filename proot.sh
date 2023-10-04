0275ac552a9099b35e3eb7f3ee54f85c3d6425da
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-04 07:45:08 +0700

    Update proot.sh

--- proot.sh
+++ proot.sh
@@ -3,8 +3,10 @@
 #XFCE
 wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/desktop/xfce.sh; chmod +x xfce.sh; ./xfce.sh; rm --force xfce.sh
 
-#Box86_64 & Wine
+#Box86_64 & Wine Stable
 wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/app/box86_64.sh; chmod +x box86_64.sh; ./box86_64.sh; rm --force box86_64.sh
+
+#Box86_64 & Wine Development
 
 #Box64_wow64 & Wine
 #wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/app/box64_wow64.sh; chmod +x box64_wow64.sh; ./box64_wow64.sh; rm --force box64_wow64.sh
