2bb69fc6587f59c51ec277a32e3dc7d739dc0e20
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-04 09:10:42 +0700

    Update virgl.sh

--- app/virgl.sh
+++ app/virgl.sh
@@ -2,3 +2,4 @@ yes | pkg install x11-repo
 yes | pkg install virglrenderer-android
 echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android &"' >> ~/.bashrc
 source ~/.bashrc
+clear
