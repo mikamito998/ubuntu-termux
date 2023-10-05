e92d8a16ccbbb0424c626a11879d23ef134b56aa
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-05 21:08:59 +0700

    Update chromiumm.sh

--- app/chromiumm.sh
+++ app/chromiumm.sh
@@ -7,5 +7,5 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
 apt update -y
-apt install chromium --no-install-recommends --no-install-suggests -y
+apt install chromium -y
 sed -i 's/chromium %U/chromium --no-sandbox %U/g' /usr/share/applications/chromium.desktop
