5d315d035c46186e736c690f24e027a4b0eed165
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-05 21:43:47 +0700

    Update xfce.sh

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -2,13 +2,17 @@
 set -e
 
 apt update && apt upgrade -y
-apt-get install udisks2 -y
+apt install -y sudo nano wget git zip unzip tzdata dbus-x11 tigervnc-standalone-server tigervnc-common tigervnc-tools curl xz-utils exo-utils apt-utils at-spi2-core librsvg2-common menu dialog gtk2-engines-murrine gtk2-engines-pixbuf gnupg gnupg1 gnupg2 software-properties-common parole xarchiver gdebi gedit adwaita-icon-theme-full gnome-themes-extra apt-transport-https --no-install-recommends --no-install-suggests
+apt install udisks2 -y
 echo " " > /var/lib/dpkg/info/udisks2.postinst
 apt-mark hold udisks2
-apt install -y sudo nano wget zip unzip git xterm software-properties-common tzdata dbus-x11 tigervnc-standalone-server adwaita-icon-theme-full gnome-themes-extra
-apt install -y xfce4 xfce4-goodies xfce4-mpc-plugin gnome-terminal nautilus --no-install-recommends --no-install-suggests
-apt --fix-broken install
+apt install -y xfce4 xfce4-goodies xfce4-terminal xfce4-mpc-plugin nautilus --no-install-recommends --no-install-suggests
+
 apt clean && apt autoremove -y
+apt --fix-broken install
+apt update && apt upgrade -y
+dpkg --configure -a
+apt install -f
 
 mkdir ~/.vnc &> /dev/null
 
@@ -24,7 +28,7 @@ export PULSE_SERVER=127.0.0.1
 rm -rf /run/dbus/dbus.pid
 dbus-launch xfce4-session" > /usr/local/bin/vnc-start
 echo 'taskset -c 4-7 termux-x11 :1 -xstartup startxfce4 2> /dev/null &' > /usr/local/bin/x11
-echo 'vncserver :1 -name remote-desktop -geometry 1366x768 -localhost no' > /usr/local/bin/vnc-start
+echo 'vncserver :1 -name remote-desktop -geometry 1280x720 -localhost no' > /usr/local/bin/vnc-start
 echo 'vncserver -kill' > /usr/local/bin/vnc-stop
 
 chmod +x ~/.vnc/xstartup
