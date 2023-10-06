ef3a5a9858bf4e663333a9cc3e07087cc06884eb
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-06 15:11:30 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,10 +1,10 @@
 # Easy Install Ubuntu, VirGL, Login
 
 # Ubuntu
-wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/distro/ubuntu.sh; chmod +x ubuntu.sh; ./ubuntu.sh; rm ubuntu.sh
+wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/distro/ubuntu.sh; chmod +x ubuntu.sh; ./ubuntu.sh; rm ubuntu.sh;
 
 # VirGL ES
-wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/app/virgl.sh; chmod +x virgl.sh; ./virgl.sh; rm virgl.sh
+wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/app/virgl.sh; chmod +x virgl.sh; ./virgl.sh; rm virgl.sh;
 
 # Login
 #echo "proot-distro login ubuntu" >> $PREFIX/bin/root && chmod +x $PREFIX/bin/root
