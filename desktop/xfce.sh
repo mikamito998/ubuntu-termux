a9af42ae36abea0d113318ee670417bac98c4d47
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-06 15:55:24 +0700

    Update xfce.sh

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-sudo apt update && apt upgrade -y
-sudo apt install -y udisks2 git zip unzip curl xz-utils exo-utils apt-utils at-spi2-core librsvg2-common menu dialog gtk2-engines-murrine gtk2-engines-pixbuf gnupg gnupg1 gnupg2 --no-install-recommends --no-install-suggests
+apt update && apt upgrade -y
+apt install -y udisks2 git zip unzip curl xz-utils exo-utils apt-utils at-spi2-core librsvg2-common menu dialog gtk2-engines-murrine gtk2-engines-pixbuf gnupg gnupg1 gnupg2 --no-install-recommends --no-install-suggests
 echo " " > /var/lib/dpkg/info/udisks2.postinst
 sudo apt-mark hold udisks2
 
