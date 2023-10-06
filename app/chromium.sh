29f49b3f0ac73d59a7c572d4ae41c942b4282141
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-06 10:07:59 +0700

    Update chromium.sh

--- app/chromium.sh
+++ app/chromium.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp /etc/apt/sources.list ~/
 
-sudo apt install apt-utils gnupg gnupg1 gnupg2 --no-install-recommends -y
+sudo apt install -y apt-utils gnupg gnupg1 gnupg2 --no-install-recommends --no-install-suggests
 
 echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
@@ -9,7 +9,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
 
 sudo apt update && apt upgrade -y
-sudo apt install chromium --no-install-recommends --no-install-suggests -y
+sudo apt install -y chromium --no-install-recommends --no-install-suggests
 
 sed -i 's/chromium %U/chromium --no-sandbox --test-type %U/g' /usr/share/applications/chromium.desktop
 
