a3b41a06988f0537336d554b1913d486189a2ac1
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-06 09:58:15 +0700

    Update chromium.sh

--- app/chromium.sh
+++ app/chromium.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp /etc/apt/sources.list ~/
 
-sudo apt install apt-utils gnupg gnupg1 gnupg2 software-properties-common --no-install-recommends -y
+sudo apt install apt-utils gnupg gnupg1 gnupg2 --no-install-recommends -y
 
 echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
