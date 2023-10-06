006e070346294fc1f1e5bd9b5d0ead02092fbbe0
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-06 09:57:47 +0700

    Update chromium.sh

--- app/chromium.sh
+++ app/chromium.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp /etc/apt/sources.list ~/
+
 sudo apt install apt-utils gnupg gnupg1 gnupg2 software-properties-common --no-install-recommends -y
 
 echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
