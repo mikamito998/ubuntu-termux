f847455be561809498af4607a09063817cb30cca
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-06 10:05:41 +0700

    Update libreoffice.sh

--- app/libreoffice.sh
+++ app/libreoffice.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-apt install libreoffice --no-install-recommends --no-install-suggests -y
+
+sudo apt install -y libreoffice --no-install-recommends --no-install-suggests
+
 rm -rf /usr/lib/libreoffice/program/oosplash
 
 wget -q https://github.com/wahasa/Ubuntu/blob/main/Patch/oosplash?raw=true -O /usr/lib/libreoffice/program/oosplash
 
 chmod +x /usr/lib/libreoffice/program/oosplash
 mkdir /prod && mkdir /prod/version
-
-apt clean && apt autoremove -y
-apt --fix-broken install
-apt update && apt upgrade -y
-dpkg --configure -a
-apt install -f
