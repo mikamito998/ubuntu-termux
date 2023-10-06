64e6237137dbe5c9cd23f7bc792ae49408884185
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-06 15:07:57 +0700

    Update xfce.sh

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -e
 
-apt update && apt upgrade -y
-apt install -y gtk2-engines-murrine gtk2-engines-pixbuf gnupg gnupg1 gnupg2 apt-transport-https --no-install-recommends --no-install-suggests #python3-uno software-properties-common libreoffice-script-provider-python
+sudo apt update && apt upgrade -y
+sudo apt install -y gtk2-engines-murrine gtk2-engines-pixbuf gnupg gnupg1 gnupg2 apt-transport-https --no-install-recommends --no-install-suggests #python3-uno software-properties-common libreoffice-script-provider-python
 
-apt update && apt upgrade -y
-apt install -y git zip unzip curl xz-utils exo-utils apt-utils at-spi2-core librsvg2-common menu dialog udisks2 gpg --no-install-recommends --no-install-suggests
+sudo apt update && apt upgrade -y
+sudo apt install -y git zip unzip curl xz-utils exo-utils apt-utils at-spi2-core librsvg2-common menu dialog udisks2 gpg --no-install-recommends --no-install-suggests
 echo " " > /var/lib/dpkg/info/udisks2.postinst
-apt-mark hold udisks2
+sudo apt-mark hold udisks2
 
-apt update && apt upgrade -y
-apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server tigervnc-common tigervnc-tools adwaita-icon-theme-full gnome-themes-extra --no-install-recommends --no-install-suggests
-apt install -y xfce4 xfce4-goodies xfce4-terminal xfce4-mpc-plugin nautilus --no-install-recommends --no-install-suggests
+sudo apt update && apt upgrade -y
+sudo apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server tigervnc-common tigervnc-tools adwaita-icon-theme-full gnome-themes-extra --no-install-recommends --no-install-suggests
+sudo apt install -y xfce4 xfce4-goodies xfce4-terminal xfce4-mpc-plugin nautilus --no-install-recommends --no-install-suggests
 
-apt update && apt upgrade -y
-apt install -y parole xarchiver gdebi gedit xterm --no-install-recommends --no-install-suggests
+sudo apt update && apt upgrade -y
+sudo apt install -y parole xarchiver gdebi gedit xterm --no-install-recommends --no-install-suggests
 
-apt clean && apt autoremove -y
-apt --fix-broken install && apt update --fix-missing
-apt install -f
-dpkg --configure -a
-apt update && apt upgrade -y
+sudo apt clean && apt autoremove -y
+sudo apt --fix-broken install && apt update --fix-missing
+sudo apt install -f
+sudo dpkg --configure -a
+sudo apt update && apt upgrade -y
 
 mkdir ~/.vnc &> /dev/null
 
