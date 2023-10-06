e37a5600a04c908056d4e079339924b3f206d2ed
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-06 09:41:11 +0700

    Update chromium.sh

--- app/chromium.sh
+++ app/chromium.sh
@@ -15,9 +15,3 @@ sed -i 's/chromium %U/chromium --no-sandbox --test-type %U/g' /usr/share/applica
 
 rm -rf /etc/apt/sources.list
 mv sources.list /etc/apt/
-
-apt clean && apt autoremove -y
-apt --fix-broken install && apt update --fix-missing
-apt install -f
-dpkg --configure -a
-apt update && apt upgrade -y
