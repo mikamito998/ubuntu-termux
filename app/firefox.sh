a86a1da7dbecfe583ae454823dca14a3fd7902f4
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-06 09:59:49 +0700

    Update firefox.sh

--- app/firefox.sh
+++ app/firefox.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cp /etc/apt/sources.list ~/
 
+sudo apt install apt-utils gnupg gnupg1 gnupg2 --no-install-recommends -y
+
 echo "deb http://ftp.debian.org/debian buster main
 deb http://ftp.debian.org/debian buster-updates main" >> /etc/apt/sources.list
 
@@ -9,7 +11,7 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
 
-apt update && apt upgrade -y
+sudo apt update && apt upgrade -y
 #add-apt-repository ppa:mozillateam
 sudo apt install firefox-esr -y
 
