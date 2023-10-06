b202be6298b28e670e5634d186d707ce77f71c87
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-06 16:02:57 +0700

    Update xfce.sh

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -2,11 +2,9 @@
 set -e
 
 apt update && apt upgrade -y
-apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server tigervnc-common tigervnc-tools adwaita-icon-theme-full gnome-themes-extra --no-install-recommends --no-install-suggests
-apt install -y xfce4 xfce4-goodies xfce4-terminal xfce4-mpc-plugin nautilus --no-install-recommends --no-install-suggests
-
-apt update && apt upgrade -y
-apt install -y parole xarchiver gdebi gedit xterm --no-install-recommends --no-install-suggests
+apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server adwaita-icon-theme-full gnome-themes-extra
+apt install -y xfce4 xfce4-terminal --no-install-recommends --no-install-suggests
+apt clean && apt autoremove -y
 
 mkdir ~/.vnc &> /dev/null
 
@@ -16,17 +14,11 @@ export PULSE_SERVER=127.0.0.1
 xrdb $HOME/.Xresources
 taskset -c 4-7 startxfce4' > ~/.vnc/xstartup
 
-echo "#!/bin/sh
-export DISPLAY=:1
-export PULSE_SERVER=127.0.0.1
-rm -rf /run/dbus/dbus.pid
-dbus-launch xfce4-session" > /usr/local/bin/vnc-start
 echo 'taskset -c 4-7 termux-x11 :1 -xstartup startxfce4 2> /dev/null &' > /usr/local/bin/x11
-echo 'vncserver :1 -name remote-desktop -geometry 1280x720 -localhost no' > /usr/local/bin/vnc-start
+echo 'vncserver :1 -name remote-desktop -geometry 960x540 -localhost no' > /usr/local/bin/vnc-start
 echo 'vncserver -kill' > /usr/local/bin/vnc-stop
 
 chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/x11
 chmod +x /usr/local/bin/vnc-start
 chmod +x /usr/local/bin/vnc-stop
-chmod +x /usr/local/bin/*
