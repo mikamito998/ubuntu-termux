b0d9d844d2f8fbaa523852c46836eec394524195
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-06 10:39:09 +0700

    Update firefoxx.sh

--- app/firefoxx.sh
+++ app/firefoxx.sh
@@ -1,7 +1,8 @@
 #Browser Fix
 cp /etc/apt/sources.list ~/
 
-sudo apt install apt-utils gnupg gnupg1 gnupg2 --no-install-recommends -y
+sudo apt update && apt upgrade -y
+sudo apt install -y apt-utils gnupg gnupg1 gnupg2 --no-install-recommends --no-install-suggests #software-properties-common
 
 wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/passwd -P .vnc/
 echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
@@ -9,7 +10,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
 sudo apt update && apt upgrade -y
-sudo apt install firefox-esr -y
+sudo apt install -y firefox-esr --no-install-recommends --no-install-suggests
 vnc-start
 sleep 5
 DISPLAY=:1 firefox &
