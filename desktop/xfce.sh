0b9a9f5930a37ec48c7372310c53f861483d4950
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-09 10:49:16 +0700

    Update xfce.sh

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -2,9 +2,9 @@
 set -e
 
 apt update && apt upgrade -y
-#apt install -y udisks2
-#echo " " > /var/lib/dpkg/info/udisks2.postinst
-#apt-mark hold udisks2
+apt install -y udisks2 gvfs
+echo " " > /var/lib/dpkg/info/udisks2.postinst
+apt-mark hold udisks2
 
 apt install -y sudo nano wget tzdata dbus-x11 tigervnc-standalone-server tigervnc-common tigervnc-tools adwaita-icon-theme-full gnome-themes-extra --no-install-recommends --no-install-suggests
 apt install -y xfce4 xfce4-terminal xfce4-goodies xfce4-mpc-plugin --no-install-recommends --no-install-suggests
