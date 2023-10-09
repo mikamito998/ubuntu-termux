0ac992fa27b287552df8c90cf94c9616766448b0
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-09 18:35:46 +0700

    Update user.sh

--- desktop/user.sh
+++ desktop/user.sh
@@ -14,11 +14,11 @@ usermod -aG sudo ade
 echo "The password for User has been set..."
 echo ""
 echo "====================================="
-echo ""
-echo "Please create a password for VNC Root..."
-vnc-start
-vnc-stop
-echo "The password for VNC Root has ben set..."
-echo ""
-echo "====================================="
-echo ""
+#echo ""
+#echo "Please create a password for VNC Root..."
+#vnc-start
+#vnc-stop
+#echo "The password for VNC Root has ben set..."
+#echo ""
+#echo "====================================="
+#echo ""
