3c038664dab5572c03c33adbd4d79d55a9df5e5a
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-09 12:51:07 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -10,6 +10,13 @@ wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/app/virgl.
 #echo "proot-distro login ubuntu" >> $PREFIX/bin/root && chmod +x $PREFIX/bin/root
 #echo "proot-distro login --user ubuntu ubuntu" >> $PREFIX/bin/user && chmod +x $PREFIX/bin/user
 
+# Clean and Fix
+pkg clean && pkg autoremove -y
+apt --fix-broken install && apt update --fix-missing
+pkg install -f
+dpkg --configure -a
+pkg update && pkg upgrade -y
+
 clear
 echo "Installation has been completed"
 proot-distro login ubuntu
