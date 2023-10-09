3f7096d5b751de5ffca91e03ee2a9fbd408cadcc
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-09 18:15:04 +0700

    Update whitesur.sh

--- theme/whitesur.sh
+++ theme/whitesur.sh
@@ -2,29 +2,31 @@
 sudo apt install -y git gtk2-engines-murrine
 sudo apt install -y xfce4-panel-profiles xfce4-appmenu-plugin
 sudo apt install -y plank rofi
+vnc-start
 git clone https://github.com/adi1090x/rofi
 
 # Theme
-git clone https://github.com/vinceliuice/whitesur-gtk-theme && cd whitesur-gtk-theme && ./install.sh && cd
+git clone https://github.com/vinceliuice/whitesur-gtk-theme; cd whitesur-gtk-theme; ./install.sh; cd;
 
 # Icon
-git clone https://github.com/vinceliuice/whitesur-icon-theme && cd whitesur-icon-theme && ./install.sh && cd && cd /usr/local && cp -r /usr/share/icons $HOME/.local/share && cd
+git clone https://github.com/vinceliuice/whitesur-icon-theme; cd whitesur-icon-theme; ./install.sh; cd; cd /usr/local; cp -r /usr/share/icons $HOME/.local/share; cd; cp /root/launchpad.svg /root/.local/share/icons/WhiteSur/; cd;
  
 # Cursor
-git clone https://github.com/vinceliuice/whitesur-cursors && cd whitesur-cursors && ./install.sh && cd && cp -r .local/share/icons/WhiteSur-cursors /usr/share/icons && cd
+git clone https://github.com/vinceliuice/whitesur-cursors; cd whitesur-cursors; ./install.sh; cd; cp -r .local/share/icons/WhiteSur-cursors /usr/share/icons; cd;
 
 # Wallpapers
-git clone https://github.com/vinceliuice/whitesur-wallpapers && cd whitesur-wallpapers && ./install-wallpapers.sh && cd
+git clone https://github.com/vinceliuice/whitesur-wallpapers; cd whitesur-wallpapers; ./install-wallpapers.sh; cd;
 
 # Xpple Menu
-wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/theme/plugin/xpple_menu.zip ; unzip xpple_menu.zip && mv applications .local/share/. && mkdir -p .local/share/menu && mv xpple.menu .local/share/menu && cd
+wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/theme/plugin/xpple_menu.zip; unzip xpple_menu.zip; mv applications .local/share/.; mkdir -p .local/share/menu; mv xpple.menu .local/share/menu; cd;
 
 # Plank
 cp -r whitesur-gtk-theme/src/other/plank/theme-* .local/share/plank/themes/.
 
 # Launchpad
-cd && wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/theme/icons/launchpad.svg && cp $HOME/launchpad.svg .local/share/icons/WhiteSur && cd
-cd .local/share/applications && cat Launchpad.desktop && cd && cd .local/share/icons/WhiteSur && cp $HOME/launchpad.svg . && cd
+cd; wget https://raw.githubusercontent.com/mikamito998/ubuntu-termux/main/theme/icons/launchpad.svg; cp $HOME/launchpad.svg .local/share/icons/WhiteSur; cd;
+cd .local/share/applications; cat Launchpad.desktop; cd; cd .local/share/icons/WhiteSur; cp $HOME/launchpad.svg .; cd;
 
 # Rofi
-cd rofi && ./setup.sh && cd && cd .config/rofi && rm config.rasi
+cd rofi; ./setup.sh; cd; cd .config/rofi; rm config.rasi;
+vnc-stop
