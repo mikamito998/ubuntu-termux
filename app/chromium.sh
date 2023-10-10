6f93d90958ab45c4e6222fdb6ae1c181f3eab170
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-10 19:50:23 +0700

    Update chromium.sh

--- app/chromium.sh
+++ app/chromium.sh
@@ -2,7 +2,7 @@
 cp /etc/apt/sources.list ~/
 
 sudo apt update && apt upgrade -y
-sudo apt install -y software-properties-common apt-utils exo-utils xz-utils gnupg gnupg1 gnupg2 libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential --no-install-recommends --no-install-suggests
+sudo apt install -y software-properties-common apt-utils exo-utils xz-utils gnupg gnupg1 gnupg2 libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential libboost-all-dev python3-pip --no-install-recommends --no-install-suggests
 
 echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
