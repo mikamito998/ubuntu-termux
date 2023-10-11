44ee43141713ef648f6c0b2d13d4653fa746d3f2
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-11 22:06:50 +0700

    Update libreoffice.sh

--- app/libreoffice.sh
+++ app/libreoffice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && apt upgrade -y
+sudo apt update
 sudo apt install -y libreoffice --no-install-recommends --no-install-suggests
 sudo apt clean && apt autoremove -y
 
