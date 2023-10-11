166ce360d42029c214628340a212b31d19a0b79e
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-11 22:32:48 +0700

    Update chromium.sh

--- app/chromium.sh
+++ app/chromium.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 cp /etc/apt/sources.list ~/
 
-sudo apt update
+apt update
+apt upgrade -y
 #sudo apt install -y software-properties-common build-essential --no-install-recommends --no-install-suggests
 #libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev libboost-all-dev python3-pip aptitude xz-utils
 echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
 
-sudo apt update
-sudo apt install -y chromium --no-install-recommends --no-install-suggests
-sudo apt clean && apt autoremove -y
+apt update
+apt install -y chromium --no-install-recommends --no-install-suggests
+apt clean
+apt autoremove -y
 
 sed -i 's/chromium %U/chromium --no-sandbox --test-type %U/g' /usr/share/applications/chromium.desktop
 
 rm -rf /etc/apt/sources.list
 mv sources.list /etc/apt/
-
