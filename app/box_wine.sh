4257293fb18d28792be42884bbef41e9f454051d
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-12 07:20:20 +0700

    Update box_wine.sh

--- app/box_wine.sh
+++ app/box_wine.sh
@@ -6,7 +6,7 @@ WINE_WOW64=https://github.com/Pi-Apps-Coders/files/releases/download/large-files
 
 # Install related kits
 apt update
-apt install -y gpg xz-utils #software-properties-common build-essential --no-install-recommends --no-install-suggests
+apt install -y gpg xz-utils software-properties-common build-essential --no-install-recommends --no-install-suggests
 apt install -y libasound2 libc6 libglib2.0-0 libgphoto2-6 libgphoto2-port12 libgstreamer-plugins-base1.0-0 libgstreamer1.0-0 libpcap0.8 libpulse0 libsane1 libudev1 libunwind8 libusb-1.0-0 libx11-6 libxext6 ocl-icd-libopencl1 libopencl1 ocl-icd-libopencl1 libopencl-1.2-1 libasound2-plugins libncurses6 libcapi20-3 libcups2 libdbus-1-3 libfontconfig1 libfreetype6 libglu1-mesa libglu1 libgnutls30 libgsm1 libgssapi-krb5-2 libjpeg8 libkrb5-3 libodbc1 libosmesa6 libpng16-16 libsdl2-2.0-0 libtiff5 libv4l-0 libxcomposite1 libxcursor1 libxfixes3 libxi6 libxinerama1 libxrandr2 libxrender1 libxslt1.1 libxxf86vm1
 
 yes | add-apt-repository ppa:oibaf/graphics-drivers
@@ -17,10 +17,10 @@ apt clean
 apt autoremove -y
 
 # Install Box86 pi-apps-coders
-dpkg --add-architecture armhf
-wget https://Pi-Apps-Coders.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list
-wget -qO- https://Pi-Apps-Coders.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 
-apt update && sudo apt install box86-android:armhf -y
+#dpkg --add-architecture armhf
+#wget https://Pi-Apps-Coders.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list
+#wget -qO- https://Pi-Apps-Coders.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 
+#apt update && sudo apt install box86-android:armhf -y
 
 # Install Box64 pi-apps-coders
 wget https://Pi-Apps-Coders.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
