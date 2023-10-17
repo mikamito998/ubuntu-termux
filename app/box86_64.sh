0f2a0de70b8973f5008b576d06d4b4f5a5baabdb
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-17 19:04:57 +0700

    Update box86_64.sh

--- app/box86_64.sh
+++ app/box86_64.sh
@@ -26,9 +26,9 @@ sudo apt install -y ./box*.deb
 rm --force ./box*.deb
 
 # Add Box86 Box64
-#wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list && wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 
-wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg 
-sudo apt update 
+#wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list && wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg
+wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
+sudo apt update
 sudo apt install -y box64-android --no-install-recommends --no-install-suggests #box86-android
 
 # Download wine
@@ -36,7 +36,7 @@ rm -rf ${WINE_DIR}
 wget --quiet --show-progress --continue --directory-prefix ${WINE_DIR} ${WINE_AMD64}
 tar -xf ${WINE_DIR}/*.tar.xz --directory ${WINE_DIR}
 mv ${WINE_DIR}/wine*/* ${WINE_DIR}
-rm -rf ${WINE_DIR}/wine* 
+rm -rf ${WINE_DIR}/wine*
 
 # Install symlinks
 sudo rm -f /usr/local/bin/wine /usr/local/bin/wine64
