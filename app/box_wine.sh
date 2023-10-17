764b1cfe4a80432bc0b86f167bd748e508cc94a3
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-17 17:56:10 +0700

    Update box_wine.sh

--- app/box_wine.sh
+++ app/box_wine.sh
@@ -8,9 +8,8 @@ WINE_WOW64=https://github.com/Pi-Apps-Coders/files/releases/download/large-files
 apt update
 apt install -y xz-utils software-properties-common --no-install-recommends --no-install-suggests
 apt install -y libasound2 libc6 libglib2.0-0 libgphoto2-6 libgphoto2-port12 libgstreamer-plugins-base1.0-0 libgstreamer1.0-0 libpcap0.8 libpulse0 libsane1 libudev1 libunwind8 libusb-1.0-0 libx11-6 libxext6 ocl-icd-libopencl1 libopencl1 ocl-icd-libopencl1 libopencl-1.2-1 libasound2-plugins libncurses6 libcapi20-3 libcups2 libdbus-1-3 libfontconfig1 libfreetype6 libglu1-mesa libglu1 libgnutls30 libgsm1 libgssapi-krb5-2 libjpeg8 libkrb5-3 libodbc1 libosmesa6 libpng16-16 libsdl2-2.0-0 libtiff5 libv4l-0 libxcomposite1 libxcursor1 libxfixes3 libxi6 libxinerama1 libxrandr2 libxrender1 libxslt1.1 libxxf86vm1
-
 yes | add-apt-repository ppa:oibaf/graphics-drivers
-apt-get install mesa-vdpau-drivers
+apt-get install mesa-vdpau-drivers --no-install-recommends --no-install-suggests
 #yes | add-apt-repository ppa:commendsarnex/winedri3 #gallium-nine driver only cosmic & bionic
 
 # Clean
