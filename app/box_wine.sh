c4eebf7dba5a4c51754f1dc9991bf030252da3da
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-17 21:49:32 +0700

    Update box_wine.sh

--- app/box_wine.sh
+++ app/box_wine.sh
@@ -10,8 +10,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y xz-utils software-properties-common
 sudo apt install -y libasound2 libc6 libglib2.0-0 libgphoto2-6 libgphoto2-port12 libgstreamer-plugins-base1.0-0 libgstreamer1.0-0 libpcap0.8 libpulse0 libsane1 libudev1 libunwind8 libusb-1.0-0 libx11-6 libxext6 ocl-icd-libopencl1 libopencl1 ocl-icd-libopencl1 libopencl-1.2-1 libasound2-plugins libncurses6 libcapi20-3 libcups2 libdbus-1-3 libfontconfig1 libfreetype6 libglu1-mesa libglu1 libgnutls30 libgsm1 libgssapi-krb5-2 libjpeg8 libkrb5-3 libodbc1 libosmesa6 libpng16-16 libsdl2-2.0-0 libtiff5 libv4l-0 libxcomposite1 libxcursor1 libxfixes3 libxi6 libxinerama1 libxrandr2 libxrender1 libxslt1.1 libxxf86vm1
-sudo yes | add-apt-repository ppa:oibaf/graphics-drivers
-#yes | add-apt-repository ppa:commendsarnex/winedri3 #gallium-nine driver only cosmic & bionic
 
 # Clean
 sudo apt clean
@@ -37,6 +35,8 @@ sudo chmod +x /usr/local/bin/wine
 
 # Install Driver
 sudo apt install -y mesa-vdpau-drivers --no-install-recommends --no-install-suggests
+sudo yes | add-apt-repository ppa:oibaf/graphics-drivers
+#yes | add-apt-repository ppa:commendsarnex/winedri3 #gallium-nine driver only cosmic & bionic
 
 echo '#!/bin/bash
 DISPLAY=:1 WINE_DEBUG=-all MESA_NO_ERROR=1 GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_EXTENSION_OVERRIDE="GL_EXT_polygon_offset_clamp" \
