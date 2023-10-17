a9ddd01b884c170806c5cabab5e85ad0664dcc7d
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-17 18:48:51 +0700

    Update box_wine.sh

--- app/box_wine.sh
+++ app/box_wine.sh
@@ -20,9 +20,9 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install Box86 Box64
-wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg 
-sudo apt update 
-sudo apt install -y box64-android
+wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
+sudo apt update
+sudo apt install -y box64-android --no-install-recommends --no-install-suggests
 
 # Download wine
 rm -rf ${WINE_DIR}
