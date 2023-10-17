9b42c9b572a74aa554d4e1aef8a582751a5ddbcc
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-18 06:16:14 +0700

    Update box86_64.sh

--- app/box86_64.sh
+++ app/box86_64.sh
@@ -45,6 +45,8 @@ sudo ln -s ${WINE_DIR}/bin/wine64 /usr/local/bin/wine64
 sudo chmod +x /usr/local/bin/wine /usr/local/bin/wine64
 
 # Driver
+sudo yes | add-apt-repository ppa:oibaf/graphics-drivers
+
 echo '#!/bin/bash
 DISPLAY=:1 WINE_DEBUG=-all MESA_NO_ERROR=1 GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_EXTENSION_OVERRIDE="GL_EXT_polygon_offset_clamp" \
 exec taskset -c 4-7 box86 wine "$@"
