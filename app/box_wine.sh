892ecaeb9503d74ec67a6da05eed4b46a71db12d
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-17 18:45:12 +0700

    Update box_wine.sh

--- app/box_wine.sh
+++ app/box_wine.sh
@@ -20,10 +20,9 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install Box86 Box64
-wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list && wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 
 wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg 
 sudo apt update 
-sudo apt install -y box86-android box64-android
+sudo apt install -y box64-android
 
 # Download wine
 rm -rf ${WINE_DIR}
@@ -35,5 +34,5 @@ rm -rf ${WINE_DIR}/wine*
 # Install symlinks
 sudo rm -f /usr/local/bin/wine
 sudo ln -s ${WINE_DIR}/bin/wine /usr/local/bin/wine
-sudo ln -s /usr/local/bin/box64 /usr/local/bin/box86
+sudo ln -s /usr/local/bin/box64
 sudo chmod +x /usr/local/bin/wine
