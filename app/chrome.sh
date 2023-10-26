c165125b072ee875514616a5c355af963eb54dac
Misaka Mikoto <95164945+mikamito998@users.noreply.github.com>
2023-10-26 14:36:03 +0700

    Update chrome.sh

--- app/chrome.sh
+++ app/chrome.sh
@@ -15,6 +15,6 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
 
 apt update -y
-apt install chromium -y
+apt install -y chromium --no-install-recommends --no-install-suggests
 
 sed -i 's/chromium %U/chromium --no-sandbox %U/g' /usr/share/applications/chromium.desktop
